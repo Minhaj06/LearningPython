0de206a18fb7343b0a38fb54ca2af10f1abb6e16
Minhaj06 <milonmdminhajkobir@gmail.com>
2022-10-09 23:52:02 +0600

    String Operations

---
+++ stringOperations.py
@@ -0,0 +1,34 @@
+txt = "I LOVE python programming"
+
+# print(txt.capitalize())
+# print(txt.casefold())
+# print(txt.lower())
+# print(txt.center(65, "_"))
+# print(txt.count("python", 2, 6))
+
+# print(txt.endswith("progring"))  # False - It's case sensitive
+# print(txt.endswith("ing"))  # True - It's case sensitive
+# print(txt.startswith("Love")) # False - It's case sensitive
+# print(txt.startswith("I LOVE"))  # True - It's case sensitive
+
+
+# name = input("Enter your name: ")
+
+# if name.startswith("Md") or name.startswith("Mr"):
+#     print("Hello, Mr.", name)
+# elif name.startswith("Mrs") or name.startswith("Ms"):
+#     print("Hello, Mrs.", name)
+# else:
+#     print("Hello,", name)
+
+# tab expanding
+# text = "\tPython"
+# print(text)
+# print(text.expandtabs(12))
+# print(text.expandtabs(16))
+# print(text.expandtabs(20))
+
+# sting finding
+# string = "Hello, Programmers"
+# print(string.find("P"))
+# print(string.rfind("o"))  # finding from end/right
